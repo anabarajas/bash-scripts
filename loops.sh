c4ed9494a8692b39e69242f917d33820f0baec75
abarajas <abarajas@hyperwallet.com>
2017-05-16 17:36:13 -0700

    first commit

---
+++ loops.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+for i in d3 hello goodbye
+do
+	echo "Looping ... random thing $i"
+done
+
+
+
+INPUT_STRING=hello
+while [ "$INPUT_STRING" != "bye" ]
+do
+  echo "Please type something in (bye to quit)"
+  read INPUT_STRING
+  echo "You typed: $INPUT_STRING"
+done
+
+
+echo "Third while loop:"
+
+while read f
+do
+  case $f in
+	hello)			echo English	;;
+	howdy)			echo American	;;
+	gday)			echo Australian	;;
+	bonjour)		echo French		;;
+	"guten tag")	echo German		;;
+	*)				echo Unknown Language: $i
+		;;
+   esac
+done < myfile
+
+while :
+do
+  echo "Please type something in (^C to quit)"
+  read INPUT_STRING
+  echo "You typed: $INPUT_STRING"
+done
